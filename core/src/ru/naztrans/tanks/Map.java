5fc3f20b60fb60f8ad6fb36f8c15c85cdb03c015
karelalex <karelalex83@gmail.com>
2018-02-18 14:20:35 +0300

    ground generation and Flying Text class added

--- core/src/ru/naztrans/tanks/Map.java
+++ core/src/ru/naztrans/tanks/Map.java
@@ -35,11 +35,48 @@ public class Map {
     }
 
     public void generate() {
+        int[] heightMap = new int[WIDTH];
+        heightMap[0] = MathUtils.random(100, HEIGHT / 5 * 3);
+        heightMap[WIDTH - 1] = MathUtils.random(100, HEIGHT / 5 * 3);
+        split(heightMap, 0, WIDTH - 1, 80, 20);
+        for (int i = 0; i < 2; i++) {
+            slideWindow(heightMap, 7);
+        }
+
         for (int i = 0; i < WIDTH; i++) {
-            for (int j = 0; j < HEIGHT / 2; j++) {
+            for (int j = 0; j < heightMap[i]; j++) {
                 data[i][j] = 1;
-                color[i][j] = ((float)j / (HEIGHT / 2));
+                color[i][j] = ((float) j / HEIGHT);
             }
+        }
+    }
+    public void split(int[] arr, int x1, int x2, int iter, int iterDecrease) {
+        int center = (x1 + x2) / 2;
+        if (iter < 0) {
+            iter = 0;
+        }
+        arr[center] = (arr[x1] + arr[x2]) / 2 + MathUtils.random(-iter, iter);
+        if (x2 - x1 > 2) {
+            split(arr, x1, center, iter - iterDecrease, iterDecrease);
+            split(arr, center, x2, iter - iterDecrease, iterDecrease);
+        }
+    }
+    public void slideWindow(int[] arr, int halfWin) {
+        for (int i = 0; i < arr.length; i++) {
+            int x1 = i - halfWin;
+            int x2 = i + halfWin;
+            if (x1 < 0) {
+                x1 = 0;
+            }
+            if (x2 > WIDTH - 1) {
+                x2 = WIDTH - 1;
+            }
+            int avg = 0;
+            for (int j = x1; j <= x2; j++) {
+                avg += arr[j];
+            }
+            avg /= (x2 - x1 + 1);
+            arr[i] = avg;
         }
     }
 
