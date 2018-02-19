0ce618f05decd30bc95fd392c7e7f792d832ec17
karelalex <karelalex83@gmail.com>
2018-02-19 21:46:12 +0300

    added some files

---
+++ android/src/ru/naztrans/tanks/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package ru.naztrans.tanks;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import ru.naztrans.tanks.TanksMain;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new TanksMain(), config);
+	}
+}
